a817d4fc67b99febced3010f81890819cfe91f6f
Senka Krivic <Senka.Krivic@uibk.ac.at>
2018-01-24 00:00:42 +0100

    change ins ervice for getting objecta nd removing all unnecessary files

--- squirrel_push_manipulation/src/retrieveObjectLocations.cpp
+++ squirrel_push_manipulation/src/retrieveObjectLocations.cpp
@@ -5,6 +5,9 @@
 #include <std_msgs/Float32.h>
 #include <string.h>
 #include <stdio.h>
+#include <iostream>
+#include <fstream>
+#include <sstream>
 
 #include <squirrel_object_perception_msgs/StartObjectTracking.h>
 #include <squirrel_object_perception_msgs/StopObjectTracking.h>
@@ -14,6 +17,7 @@
 #include <tf/transform_listener.h>
 
 using namespace std;
+std::string getexepath();
 
 string global_frame_ ="map";
 
@@ -35,45 +39,82 @@ bool getPos(squirrel_manipulation_msgs::GetObjectPositions::Request  &req,
     res.objectids.resize(obj_num);
     res.objectposes.resize(obj_num);
 
-    tf::TransformListener tf_listener_;
-    for(int i = 0; i<obj_num; i++){
-        res.diameters[i] = diameters[i];
-        res.objectids[i] = objects[i];
-        string object_id_ =  objects[i];
-        tf::StampedTransform trans;
-        geometry_msgs::PoseStamped pose_object_;
+    //tf::TransformListener tf_listener_;
+    //for(int i = 0; i<obj_num; i++){
+    //        res.diameters[i] = diameters[i];
+    //        res.objectids[i] = objects[i];
+    //        string object_id_ =  objects[i];
+    //        tf::StampedTransform trans;
+    geometry_msgs::PoseStamped pose_object_;
 
-        //start tracking
-        squirrel_object_perception_msgs::StartObjectTracking srvStartTrack;
-        srvStartTrack.request.object_id.data = object_id_;
-        ROS_INFO("(Get poses) Waiting for the tracker of the %s to start \n", object_id_.c_str());
+    string path = getexepath();
+    //cout<<path<<endl;
 
-        if(!ros::service::call("/squirrel_start_object_tracking", srvStartTrack)){
-            ROS_ERROR("(Get poses) Start tracking of the %s failed \n", object_id_.c_str());
+    string p = "devel/.private/squirrel_push_manipulation/lib/squirrel_push_manipulation/getobj";
+    string base = path.substr(0, path.length() - p.length());
+    string path_file = base+"src/squirrel_manipulation/data/object_locations.txt";
+    //cout<<path_file<<endl;
+    ifstream infile;
+    infile.open(path_file.c_str());
+    if (infile.is_open()){
+        string line;
+        std::string obj;
+        double x, y, z;
+
+        int line_count = 0;
+        while (getline(infile,line)) {
+            std::istringstream in(line);
+            in >> obj >> x >> y >> z;
+            pose_object_.pose.position.x = x;
+            pose_object_.pose.position.y = y;
+            pose_object_.pose.position.y = z;
+            res.objectposes[line_count] = pose_object_.pose;
+            res.diameters[line_count] = diameters[line_count];
+            res.objectids[line_count] = objects[line_count];
+            //cout<<pose_object_<<endl;
+            line_count++;
+
         }
-
-        try {
-            tf_listener_.waitForTransform(global_frame_, object_id_, ros::Time::now(), ros::Duration(10.0));
-            tf_listener_.lookupTransform(global_frame_, object_id_, ros::Time(0), trans);
-            pose_object_ = tf_stamped2pose_stamped(trans);
-        } catch (tf::TransformException& ex) {
-            std::string ns = ros::this_node::getNamespace();
-            std::string node_name = ros::this_node::getName();
-            ROS_ERROR("(Get poses) %s/%s: %s", ns.c_str(), node_name.c_str(), ex.what());
-        }
-        res.objectposes[i] = pose_object_.pose;
-
-        //stop tracking
-
-        squirrel_object_perception_msgs::StopObjectTracking srvStopTrack;
-        ros::service::call("/squirrel_stop_object_tracking", srvStopTrack);
-        ROS_INFO("(Get poses) tracking of %s stopped \n", object_id_.c_str());
-
     }
+
+    //start tracking
+    //        squirrel_object_perception_msgs::StartObjectTracking srvStartTrack;
+    //        srvStartTrack.request.object_id.data = object_id_;
+    //        ROS_INFO("(Get poses) Waiting for the tracker of the %s to start \n", object_id_.c_str());
+
+    //        if(!ros::service::call("/squirrel_start_object_tracking", srvStartTrack)){
+    //            ROS_ERROR("(Get poses) Start tracking of the %s failed \n", object_id_.c_str());
+    //        }
+
+    //        try {
+    //            tf_listener_.waitForTransform(global_frame_, object_id_, ros::Time::now(), ros::Duration(10.0));
+    //            tf_listener_.lookupTransform(global_frame_, object_id_, ros::Time(0), trans);
+    //            pose_object_ = tf_stamped2pose_stamped(trans);
+    //        } catch (tf::TransformException& ex) {
+    //            std::string ns = ros::this_node::getNamespace();
+    //            std::string node_name = ros::this_node::getName();
+    //            ROS_ERROR("(Get poses) %s/%s: %s", ns.c_str(), node_name.c_str(), ex.what());
+    //        }
+    // res.objectposes[i] = pose_object_.pose;
+
+    //stop tracking
+
+    //        squirrel_object_perception_msgs::StopObjectTracking srvStopTrack;
+    //        ros::service::call("/squirrel_stop_object_tracking", srvStopTrack);
+    //        ROS_INFO("(Get poses) tracking of %s stopped \n", object_id_.c_str());
+
+    //}
 
 
     return true;
 
+}
+
+std::string getexepath()
+{
+    char result[ PATH_MAX ];
+    ssize_t count = readlink( "/proc/self/exe", result, PATH_MAX );
+    return std::string( result, (count > 0) ? count : 0 );
 }
 
 int main(int argc, char **argv)
